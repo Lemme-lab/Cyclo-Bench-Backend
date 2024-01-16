206ccf5cf8a26d74ae55dfe551de9239d798920d
lemme <Lucas.lenarcic@gmail.com>
2024-01-16 14:17:41 +0100

    trying to fix bug

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
-import express from "express";
+import express, {
+    request
+} from "express"
+import {
+    PORT,
+    mongoDBURL
+} from "./config.js";
 import mongoose from "mongoose";
-import cors from 'cors';
-import { PORT, mongoDBURL } from "./config.js";
 import routerSensorData from './routes/SensorDataRouter.js';
 import routerControll from './routes/ControllStateRouter.js';
+import cors from 'cors';
 
 const app = express();
 
-// Use CORS middleware with specific options
-const corsOptions = {
-  origin: 'http://10.68.14.105:3333', 
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
 app.use(express.json());
+
+app.use(cors());
+
 app.use('/Data', routerSensorData);
+
 app.use('/Controll', routerControll);
 
-mongoose.connect(mongoDBURL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('App Connected to Database');
-    console.log('On Port: ' + PORT);
-    app.listen(PORT, () => {
-      console.log("App is listening");
-    });
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+
+mongoose.connect(mongoDBURL)
+    .then(() => {
+        console.log('App Connected to Database');
+        console.log('On Port: ' + PORT);
+        app.listen(PORT,  () => {
+            console.log("App is listening")
+        });
+    }).catch((error) => {
+        console.log(error);
+    })
