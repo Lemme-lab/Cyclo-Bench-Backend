88b2752bb422ec22cc944d8aba9609e6a194bb26
lemme <Lucas.lenarcic@gmail.com>
2024-01-16 13:56:24 +0100

    trying to fix api error on raspberry pi

--- index.js
+++ index.js
@@ -1,36 +1,32 @@
-import express, {
-    request
-} from "express"
-import {
-    PORT,
-    mongoDBURL
-} from "./config.js";
+import express from "express";
 import mongoose from "mongoose";
+import cors from 'cors';
+import { PORT, mongoDBURL } from "./config.js";
 import routerSensorData from './routes/SensorDataRouter.js';
 import routerControll from './routes/ControllStateRouter.js';
-import cors from 'cors';
 
 const app = express();
 
+// Use CORS middleware with specific options
+const corsOptions = {
+  origin: 'http://localhost:3333', // replace with your actual frontend origin
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
-
-app.use(cors());
-
 app.use('/Data', routerSensorData);
-
 app.use('/Controll', routerControll);
 
-
-
-
-
-mongoose.connect(mongoDBURL)
-    .then(() => {
-        console.log('App Connected to Database');
-        console.log('On Port: ' + PORT);
-        app.listen(PORT,  () => {
-            console.log("App is listening")
-        });
-    }).catch((error) => {
-        console.log(error);
-    })
+mongoose.connect(mongoDBURL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('App Connected to Database');
+    console.log('On Port: ' + PORT);
+    app.listen(PORT, () => {
+      console.log("App is listening");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
