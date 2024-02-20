8bd1c4064aeeb3f143912a0d4063c51e7bbdc6a1
lemme <Lucas.lenarcic@gmail.com>
2024-02-20 09:38:26 +0100

    fixing bug

--- index.js
+++ index.js
@@ -9,13 +9,10 @@ const app = express();
 
 app.use(express.json());
 
-// Configure CORS middleware to allow cross-origin requests from http://localhost:3000
-app.use(cors({
-    origin: 'http://localhost:80'
-}));
+app.use(cors());
 
-// Define routes for sensor data and control state
 app.use('/Data', routerSensorData);
+
 app.use('/Controll', routerControll);
 
 // Middleware to log request details
@@ -24,13 +21,11 @@ app.use((req, res, next) => {
     next();
 });
 
-// Connect to MongoDB database
 mongoose.connect(mongoDBURL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('App Connected to Database');
         console.log('MongoDB URL:', mongoDBURL);
         console.log('On Port:', PORT);
-        // Start the server
         app.listen(PORT, '0.0.0.0', () => {
             console.log("App is listening");
             console.log("Server started at:", new Date().toLocaleString());
@@ -44,3 +39,4 @@ app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
 });
+
